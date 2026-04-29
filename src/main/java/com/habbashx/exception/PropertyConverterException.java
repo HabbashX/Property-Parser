07d66c41afeef3b5ce73c1ef7fa47697c9317c76
HabbashX <abdalahalhabbash@gmail.com>
2026-04-29 14:28:59 +0300

    ,

---
+++ src/main/java/com/habbashx/exception/PropertyConverterException.java
@@ -0,0 +1,9 @@
+package com.habbashx.exception;
+
+
+public class PropertyConverterException extends RuntimeException {
+
+    public PropertyConverterException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/habbashx/exception/PropertyDecryptorException.java
@@ -0,0 +1,8 @@
+package com.habbashx.exception;
+
+public class PropertyDecryptorException extends RuntimeException {
+
+    public PropertyDecryptorException(String message) {
+        super(message);
+    }
+}
